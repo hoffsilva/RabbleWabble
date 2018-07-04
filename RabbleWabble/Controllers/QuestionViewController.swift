ae10be7ce78750ac43f26862a6c9a9d0493a8232
Alelo <cast@casts-macbook-pro.local>
2018-07-04 07:53:52 -0300

    Finalização do capitulo de MVC.

--- RabbleWabble/Controllers/QuestionViewController.swift
+++ RabbleWabble/Controllers/QuestionViewController.swift
@@ -12,6 +12,59 @@ class QuestionViewController: UIViewController {
 
     //MARK - Instance Properties
     public var questionGroup = QuestionGroup.basicPhrases()
+    public var questionIndex = 0
+    public var correctCount = 0
+    public var incorrectCount = 0
+    
+    public var questionView: QuestionView! {
+        guard isViewLoaded else {
+            return nil
+        }
+        return view as! QuestionView
+    }
+    
+    //MARK - View Lificycle
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        showQuestion()
+    }
+    
+    private func showQuestion() {
+        let question = questionGroup.questions[questionIndex]
+        
+        questionView.answerLabel.text = question.answer
+        questionView.promptLabel.text = question.prompt
+        questionView.hintLabel.text = question.hint
+        
+        questionView.answerLabel.isHidden = true
+        questionView.hintLabel.isHidden = true
+    }
+    
+    private func showNextQuestion() {
+        questionIndex += 1
+        guard questionIndex < questionGroup.questions.count else {
+            
+            return
+        }
+        showQuestion()
+    }
+    
+    @IBAction func toggleAnswerLabels(_ sender: Any) {
+        questionView.answerLabel.isHidden = !questionView.answerLabel.isHidden
+        questionView.hintLabel.isHidden = !questionView.hintLabel.isHidden
+    }
+    
+    @IBAction func handleCorrect(_ sender: Any) {
+        correctCount += 1
+        questionView.correctCountLabel.text = "\(correctCount)"
+        showQuestion()
+    }
+    
+    @IBAction func handleIncorrect(_ sender: Any) {
+        incorrectCount += 1
+        questionView.incorrectCountLabel.text = "\(incorrectCount)"
+        showNextQuestion()
+    }
     
 }
 
